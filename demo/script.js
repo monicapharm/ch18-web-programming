491a699bdb6bd4b1d1f5cc9181a705f7f772b1c3
monicapharm <maliuqing123@gmail.com>
2018-05-23 12:16:11 -0700

    Added code in demo and exercise-2

--- demo/script.js
+++ demo/script.js
@@ -1,3 +1,11 @@
 'use strict';
 //`d3` has already been "imported"
 
+var heading = d3.select("h1");
+console.log(heading.html());
+
+var element = d3.select("#lorem");
+console.log(element.html());
+
+var strong =  element.select("strong")
+console.log(strong.html());
